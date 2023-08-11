421f856597b6611cddd6d322106b40afd8c9b5a3
vinicius <vinicius.merelis@gmail.com>
2023-08-11 10:24:20 -0300

    Adicionando alteracoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthConfig, NullValidationHandler, OAuthService} from "angular-oauth2-oidc";
+import {ClienteService} from "./cliente.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-oauth2';
+
+  constructor(private oauthService: OAuthService, private clienteService: ClienteService) {
+    this.configure();
+  }
+
+  authConfig: AuthConfig = {
+    issuer: 'http://localhost:8081/realms/agenda',
+    redirectUri: window.location.origin,
+    clientId: 'agenda-client-web',
+    responseType: 'code',
+    scope: 'openid profile email offline_access',
+    showDebugInformation: true,
+  };
+
+  configure(): void {
+    this.oauthService.configure(this.authConfig);
+    this.oauthService.tokenValidationHandler = new NullValidationHandler();
+    this.oauthService.setupAutomaticSilentRefresh();
+    this.oauthService.loadDiscoveryDocument().then(() => this.oauthService.tryLogin());
+  }
+
+  login(): void {
+    this.oauthService.initImplicitFlowInternal();
+  }
+
+  logout(): void {
+    this.oauthService.logOut();
+  }
+
+  listarClientes() {
+    this.clienteService.listar().subscribe(resp => console.log(resp));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {OAuthModule} from "angular-oauth2-oidc";
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['/api'],
+        sendAccessToken: true
+      }
+    }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
